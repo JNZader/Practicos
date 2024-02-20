464d4a02df00483d8771062dd60de1d9a9af9591
JNZader <javierzader@hotmail.com>
2024-02-20 16:04:55 -0300

    7

---
+++ JavaScript/TPO1/PrácticoA/Ej7/script.js
@@ -0,0 +1,3 @@
+let frases=["Lorem ipsum dolor sit amet consectetur adipisicing elit.", "Ullam atque quo eius odio deleniti eligendi obcaecati sapiente quisquam numquam","ad beatae unde et corporis maiores magnam, sequi quis minus cum?"];
+
+document.getElementById("random").innerHTML=(frases[Math.floor(Math.random()*frases.length)]);
